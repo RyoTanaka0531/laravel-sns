32d2cf4166a68ebd0a29863ab0acda9bb81c846b
tanakaryo <ch5cxcxxh@gmail.com>
2020-09-29 14:40:46 +0900

    add articleRequest

---
+++ app/Http/Requests/ArticleRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ArticleRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    //指定されたリソースを更新する権限を持っているか確認するメソッド
+    public function authorize()
+    {
+        //他の場所で認証ロジックを行う設計をする場合はtrueを返すように設定
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            //
+            'title' => 'required|max:150',
+            'body' => 'required|max:500',
+        ];
+    }
+
+    //attributesメソッドでは、バリデーションエラーメッセージに表示される項目名をカスタマイズできる
+    public function attributes()
+    {
+        return[
+            'title' => 'タイトル',
+            'body' => '本文',
+        ]
+    }
+}
