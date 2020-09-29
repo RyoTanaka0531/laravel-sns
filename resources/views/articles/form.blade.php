316df7f34a7ee160d9120cf816e5f1f12340255f
tanakaryo <ch5cxcxxh@gmail.com>
2020-09-29 16:35:37 +0900

    add modal

--- resources/views/articles/form.blade.php
+++ resources/views/articles/form.blade.php
@@ -1,9 +1,10 @@
 @csrf
 <div class="md-form">
     <label>タイトル</label>
-    <input type="text" name="title" class="form-control" required value="{{old('title')}}">
+    {{-- null合体演算子は 式1 ?? 式２ の形式で記述し、式1がnullでない場合、式1が結果となり、式1がnullの場合、式2が結果となる --}}
+    <input type="text" name="title" class="form-control" required value="{{$article->title ?? old('title')}}">
 </div>
 <div class="form-group">
     <label></label>
-    <textarea name="body" required class="form-control" row="16" placeholder="本文">{{old('body')}}</textarea>
+    <textarea name="body" required class="form-control" row="16" placeholder="本文">{{$article->body ?? old('body')}}</textarea>
 </div>
