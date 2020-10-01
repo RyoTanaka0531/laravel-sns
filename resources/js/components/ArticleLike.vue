82aac4546501df26c62cc158d0473d24b78f746e
tanakaryo <ch5cxcxxh@gmail.com>
2020-10-01 14:21:55 +0900

    add animatedicon

--- resources/js/components/ArticleLike.vue
+++ resources/js/components/ArticleLike.vue
@@ -6,7 +6,7 @@
         >
             <!-- プロパティの値がtrueの時、キーのクラスが適用される -->
             <i class="fas fa-heart mr-1"
-            :class="{'red-text':this.isLikedBy}"
+            :class="{'red-text':this.isLikedBy, 'animated heartBeat fast':this.gotToLike}"
             @click="clickLike"
             />
         </button>
@@ -37,6 +37,7 @@
         return {
             isLikedBy: this.initialIsLikedBy,
             countLikes: this.initialCountLikes,
+            gotToLike: false,
             }
         },
         methods:{
@@ -54,12 +55,14 @@
 
                 this.isLikedBy = true
                 this.countLikes = response.data.countLikes
+                this.gotToLike = true
             },
             async unlike() {
                 const response = await axios.delete(this.endpoint)
 
                 this.isLikedBy = false
                 this.countLikes = response.data.countLikes
+                this.gotToLike = false
             },
         },
     }
