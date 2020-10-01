8f7673c5712c3e1bd396bc7ee120fbf55b7534ef
tanakaryo <ch5cxcxxh@gmail.com>
2020-10-01 13:35:46 +0900

    edit controller

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ Route::get('/', 'ArticleController@index')->name('articles.index');
 //middlewareを使い、未ログインユーザーのurl直打ちでの遷移を拒否している
 Route::resource('/articles', 'ArticleController')->except(['index', 'show'])->middleware('auth');
 Route::resource('/articles', 'ArticleController')->only(['show']);
+//prefixメソッドは引数として渡した文字列をURIの先頭につける
+Route::prefix('articles')->name('articles.')->group(function(){
+    Route::put('/{article}/like', 'ArticleController@like')->name('like')->middleware('auth');
+    Route::delete('/{article}/like', 'ArticleController@unlike')->name('unlike')->middleware('auth');
+});
