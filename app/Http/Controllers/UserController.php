3ec4600819b2638bb6fdeb8f2a58958ab7e67135
tanakaryo <ch5cxcxxh@gmail.com>
2020-10-03 12:42:21 +0900

    view followings

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -51,4 +51,18 @@ class UserController extends Controller
         $articles = $user->likes->sortByDesc('created_at');
         return view('users.likes', ['user' => $user, 'articles' => $articles]);
     }
+
+    public function followings(string $name)
+    {
+        $user = User::where('name', $name)->first();
+        $followings = $user->followings->sortByDesc('created_at');
+        return view('users.followings', ['user' => $user, 'followings' => $followings]);
+    }
+
+    public function followers(string $name)
+    {
+        $user = User::where('name', $name)->first();
+        $followers = $user->followers->sortByDesc('create_at');
+        return view('users.followers', ['user' => $user, 'followers' => $followers]);
+    }
 }
