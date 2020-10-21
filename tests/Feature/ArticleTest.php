61c034b287829027608ac316084a17795c92acc9
tanakaryo <ch5cxcxxh@gmail.com>
2020-10-21 22:24:42 +0900

    edit articletest

--- tests/Feature/ArticleTest.php
+++ tests/Feature/ArticleTest.php
@@ -5,27 +5,18 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
-use App\User;
-use App\Article;
+
 
 class ArticleTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function testExample()
+    // use RefreshDatabase;
+
+    public function testIndex()
     {
-        $response = $this->get('/');
+        $response = $this->get(route('articles.index'));
 
-        $response->assertStatus(200);
-
-        $user = factory(User::class)->create();
-        $response = $this->actingAs($user)->get('articles/create');
-        // $article = factory(Article::class)->create();
-        // $response = $this->actingAs($user)->get('articles/{article}/edit');
-
-        $response->assertStatus(200);
+        $response->assertStatus(200)
+            ->assertViewIs('artilces.index');
     }
+
 }
